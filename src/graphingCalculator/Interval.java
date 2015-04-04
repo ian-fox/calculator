4e75da04669156806f74d2375cf3f536b469462b
Ian Fox <ianfox97@gmail.com>
2015-04-04 14:46:19 -0400

    Added intervals

---
+++ src/graphingCalculator/Interval.java
@@ -0,0 +1,58 @@
+package graphingCalculator;
+
+class Interval {
+    double start, end;
+    boolean includeStart, includeEnd;
+    boolean infStart = false;
+    boolean infEnd = false;
+    
+    public Interval (char inclStart, double start, double end, char inclEnd) {
+        this.start = start;
+        this.end = end;
+        includeStart = inclStart == '[';
+        includeEnd = inclEnd == ']';
+    }
+    
+    public Interval (double end, char inclEnd) {
+        this.end = end;
+        infStart = true;
+        includeStart = false;
+        includeEnd = inclEnd == ']';
+    }
+    
+    public Interval (char inclStart, double start) {
+        this.start = start;
+        includeStart = inclStart == '[';
+        infEnd = true;
+        includeEnd = false;
+    }
+    
+    public Interval () {
+        infStart = true;
+        includeStart = false;
+        infEnd = true;
+        includeEnd = false;
+    }
+    
+    public boolean isInInterval (double d) {
+        if (infStart && infEnd) return true;
+        if (infStart && includeEnd) return d <= end;
+        if (infStart) return d < end;
+        if (includeStart && infEnd) return d >= start;
+        if (infEnd) return d > start;
+        if (includeStart && includeEnd) return d >= start && d <= end;
+        if (includeStart) return d >= start && d < end;
+        if (includeEnd) return d > start && d <= end;
+        return d > start && d < end;
+    }
+    
+    public String toString() {
+        String s = "";
+        s += includeStart ? "[" : "(";
+        s += infStart ? "-inf" : start;
+        s += ", ";
+        s += infEnd ? "inf" : end;
+        s += includeEnd ? "]" : ")";
+        return s;
+    }
+}
